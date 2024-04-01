25d7827692bc008e801a4b88d155b0d73d9866b5
Ahmet Ali <ahmethocam@gmail.com>
2024-04-01 16:44:32 +0300

    MultiAuth Denemesi

---
+++ app/Models/Veli.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Veli extends Model
+{
+    use HasFactory;
+}
